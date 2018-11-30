1b70b5a6a5378d4e2d474e07ed908ae0054a76c0
William Ackemo <william.ackemo@hotmail.com>
2018-11-30 13:44:38 +0100

    Created Pet project

---
+++ Pets-Ackemo/Pets-Ackemo/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pets_Ackemo
+{
+    class Pet
+    {
+        public string name { get; set; }
+
+        public string PetInfo()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ Pets-Ackemo/Pets-Ackemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pets_Ackemo
+{
+    class Program
+    {
+        public static List<Pet> PetList = new List<Pet>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WE GOT PETS! LOOK AT THEM");
+
+            Console.WriteLine("");
+            
+            Console.ReadKey();
+
+        }
+
+        public static void AddPet()
+        {
+            string name = Console.ReadLine();
+            Pet pet = new Pet();
+            pet.name = name;
+            PetList.Add(pet);
+        }
+    }
+}
